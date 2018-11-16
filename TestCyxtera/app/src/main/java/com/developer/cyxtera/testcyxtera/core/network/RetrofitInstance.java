e479fc18b06a70fd1e17f0f7bf824161a68dbacc
desarrollo <fabian.madero@outlook.com>
2018-11-16 11:40:48 -0500

    Primer commit

---
+++ TestCyxtera/app/src/main/java/com/developer/cyxtera/testcyxtera/core/network/RetrofitInstance.java
@@ -0,0 +1,23 @@
+package com.developer.cyxtera.testcyxtera.core.network;
+
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+public class RetrofitInstance {
+
+    private static Retrofit retrofit;
+    private static final String BASE_URL = "http://api.geonames.org/timezoneJSON/";
+
+    /**
+     * Creando la instancia de retrofit
+     * */
+    public static Retrofit getRetrofitInstance() {
+        if (retrofit == null) {
+            retrofit = new retrofit2.Retrofit.Builder()
+                    .baseUrl(BASE_URL)
+                    .addConverterFactory(GsonConverterFactory.create())
+                    .build();
+        }
+        return retrofit;
+    }
+}
---
+++ TestCyxtera/testcyxteralibrary/src/test/java/com/developer/cyxtera/testcyxteralibrary/LoginTest.java
@@ -0,0 +1,68 @@
+package com.developer.cyxtera.testcyxteralibrary;
+
+import com.developer.cyxtera.testcyxteralibrary.features.login.features.login.data.entities.User;
+import com.developer.cyxtera.testcyxteralibrary.features.login.features.login.ui.LoginPresenter;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static junit.framework.TestCase.assertTrue;
+import static org.junit.Assert.assertFalse;
+
+public class LoginTest {
+
+    private LoginPresenter loginPresenter;
+
+    @Before
+    public void setUp() throws Exception {
+        loginPresenter = new LoginPresenter();
+    }
+
+    @Test
+    public void validateDataWithResultTrue() {
+        String usuario = "famara@gmail.com";
+        String password = "java1ABC$";
+        boolean resultado = loginPresenter.validateData(createUser(usuario,password));
+        assertTrue(resultado);
+    }
+
+    @Test
+    public void validateDataResultFalseForUser() {
+        String usuario = "famagmadfdfdfdffdf.com";
+        String password = "java1ABC$";
+        boolean resultado = loginPresenter.validateData(createUser(usuario,password));
+        assertFalse(resultado);
+    }
+
+    @Test
+    public void validateDataWithResultFalseForPassword() {
+        String usuario = "famara@gmail.com";
+        String password = "123a.";
+        boolean resultado = loginPresenter.validateData(createUser(usuario,password));
+        assertFalse(resultado);
+    }
+
+    @Test
+    public void validateDataWithResultFalseForUserisEmpty() {
+        String usuario = "";
+        String password = "java1ABC$";
+        boolean resultado = loginPresenter.validateData(createUser(usuario,password));
+        assertFalse(resultado);
+    }
+
+    @Test
+    public void validateDataWithResultFalseForPasswordisEmpty() {
+        String usuario = "famara@gmail.com";
+        String password = "";
+        boolean resultado = loginPresenter.validateData(createUser(usuario,password));
+        assertFalse(resultado);
+    }
+
+    private User createUser(String email, String password){
+        User user = new User();
+        user.setEmail(email);
+        user.setPassword(password);
+        return user;
+    }
+
+}
