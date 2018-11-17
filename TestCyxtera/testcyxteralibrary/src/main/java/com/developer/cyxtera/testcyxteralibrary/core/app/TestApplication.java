fdc4031ead79585d8964adc8112f8ff15533c8df
desarrollo <fabian.madero@outlook.com>
2018-11-16 20:58:56 -0500

    tercer commit

--- TestCyxtera/testcyxteralibrary/src/main/java/com/developer/cyxtera/testcyxteralibrary/core/app/TestApplication.java
+++ TestCyxtera/testcyxteralibrary/src/main/java/com/developer/cyxtera/testcyxteralibrary/core/app/TestApplication.java
@@ -2,6 +2,7 @@ package com.developer.cyxtera.testcyxteralibrary.core.app;
 
 import android.app.Application;
 
+import com.developer.cyxtera.testcyxteralibrary.core.database.greendao.DaoMaster;
 import com.developer.cyxtera.testcyxteralibrary.core.database.greendao.DaoSession;
 
 
@@ -11,6 +12,19 @@ import com.developer.cyxtera.testcyxteralibrary.core.database.greendao.DaoSessio
 public class TestApplication extends Application {
 
     private DaoSession mDaoSession;
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        if(mDaoSession == null) {
+            mDaoSession = new DaoMaster(
+                    new DaoMaster.DevOpenHelper(this, "greendao_demo.db").getWritableDb()).newSession();
+        }
+    }
+
+    public DaoSession getDaoSession() {
+        return mDaoSession;
+    }
 
 
 
